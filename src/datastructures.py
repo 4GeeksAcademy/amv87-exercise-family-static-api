f28379eaa7d33ab033df66d6285cb7fd6f371409
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-14 21:33:49 +0000

    changes to src/datastructures.py

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,58 +1,49 @@
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        self._members = [
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        },
             {
-                "id": self._generateId(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7,13,22]
-            },
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        },
             {
-                "id": self._generateId(),
-                "first_name": "Jane",
-                "last_name": last_name,
-                "age": 35,
-                "lucky_numbers": [3,10,14]
-            },
-            {
-                "id": self._generateId(),
-                "first_name": "Jimmy",
-                "last_name": last_name,
-                "age": 5,
-                "lucky_numbers": [1]
-            }
-        ]
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [1,3,4]
+        }]
 
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        new_member = {
-            "id": self._generateId(),
-            "first_name": member["first_name"],
-            "last_name": self.last_name,
-            "age": 5,
-            "lucky_numbers": member["lucky_numbers"]
-        }
-
-        self._members.append(new_member)
-        return self._members
+       self._members.append(member)
+       return self._members
 
     def delete_member(self, id):
         for member in self._members:
             if member["id"] == id:
                 self._members.remove(member)
-        return self._members
+                return member 
 
     def get_member(self, id):
         for member in self._members:
             if member["id"] == id:
                 return member
-
+       
     def get_all_members(self):
         return self._members
